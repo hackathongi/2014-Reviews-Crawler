732f8eb7797efc6d12cf3709c40d3b5098358b42
Alex Toro <alex@volcanicinternet.com>
2014-03-08 10:30:13 +0100

    Add .gitignore

---
+++ Constants.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alex@volcanicinternet.cat
+ * Date: 3/8/14
+ * Time: 9:53 AM
+ */
+
+//MAX_RATING
+define('MAX_RATING', 'max_rating');
+
+//SHOP
+define('SHOP_NAME', 'shop_name');
+define('SHOP_ADDRESS', 'address');
+define('SHOP_URL', 'shop_url');
+define('SHOP_URL_COMPETITOR', 'shop_url_competitor');
+define('SHOP_ID', 'shop_id');
+define('SHOP_PHONE', 'shop_phone');
+define('SHOP_EMAIL', 'shop_email');
+define('SHOP_LOGO', 'shop_logo');
+
+//REVIEW
+define('DESCRIPTION', 'description');
+define('RATING', 'rating');
+define('DATE', 'date');
+define('LANGUAGE', 'language');
+
+//USER
+define('USER_NAME', 'name');
+define('USER_SURNAME', 'surname');
+define('USER_EMAIL', 'user_email');
+
+?>
