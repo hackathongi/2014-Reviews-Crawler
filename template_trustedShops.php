4cc16d61f6006d42c8f2480380939cdb6bbb834d
Alex Toro <alex@volcanicinternet.com>
2014-03-08 17:40:43 +0100

    Add .gitignore

--- template_trustedShops.php
+++ template_trustedShops.php
@@ -10,67 +10,77 @@
 include_once 'constants.php';
 include_once 'htmlDownloader.php';
 
-class eKomiImporter
+class trustedShopsImporter
 {
-    public $encoding = 'Windows-1252';
+    public $encoding = 'UTF-8';
     public $reviews = array();
     public $shop = array();
-    public $competitorMaxRate = 10;
+    public $competitorMaxRate = 5;
     public $languange = '';
 
     public function run($url_competitor, $shop_id = '')
 	{
-
         echo "\nBOT START\n\n";
 
         //DOWNLOAD MAIN HTML
         $downloader = new htmlDownloader();
         $mainUrl = $url_competitor;
         $mainHtml = $downloader->get_html($mainUrl);
-
-        $shopHtml = $this->encodeHTML($mainHtml);
-        if (preg_match('|xml:lang="([^"]*)"|', $shopHtml, $m)){
+        if (preg_match('|<meta name="language" content="([^"]*)"|', $mainHtml, $m)){
             $this->languange = $m[1];
         }
+
+        $shopUrl = str_replace('evaluacion', 'perfil', $mainUrl);
+        $shopHtml2 = $downloader->get_html($shopUrl);
+        $shopHtml = $this->encodeHTML($mainHtml);
         //SHOP
-        if (preg_match('|<h2 [^>]*class="sectionTitle"[^>]*>[^<]*<span[^>]*>([^<]*)</span>|', $shopHtml, $m)){
+        if (preg_match('|<h1><span itemprop="name">(.*)</span>|isU', $shopHtml, $m)){
             $this->shop[SHOP_NAME] = $m[1];
         }
-        if (preg_match('|<p class="shopAddressDetails">(.*)</p>|isU', $shopHtml, $m)){
-            $this->shop[SHOP_ADDRESS] = $this->myStripTags($m[1]);
+        if (preg_match('|<a href="[^"]*/certificate\.php[^>]*>[^<]*</a>\s*</td>\s*</tr>\s*<tr>\s*<td><strong>[^<]*</strong></td>\s*<td>(.*)</td>|', $shopHtml, $m)){
+            $this->shop[SHOP_ADDRESS] = $m[1];
         }
-        if (preg_match('|<div[^>]*>Website:</div>.*<a class="shoplink url" href=[\'"]([^\'"]*)[\'"]|isU', $shopHtml, $m)){
+        if (preg_match('|<a target="_blank" class="url" href="([^"]*)"|', $shopHtml, $m)){
             $this->shop[SHOP_URL] = $m[1];
         }
         $this->shop[SHOP_URL_COMPETITOR] = $url_competitor;
         if ($shop_id) $this->shop[SHOP_ID] = $shop_id;
-        if (preg_match('|<strong>Tel:</strong>([^<]*)<|isU', $shopHtml, $m)){
-            $this->shop[SHOP_PHONE] = $m[1];
+
+        if (preg_match('|<div class="contact">\s*<p>[^<]*</p>\s*<ul>\s*<li>\s*<a[^>]*>([^<]*)</a>\s*</li>\s*<li>([^<]*)</li>|isU', $shopHtml2, $m)){
+            $this->shop[SHOP_EMAIL] = $m[1];
+            $this->shop[SHOP_PHONE] = $m[2];
+        }
+        elseif (preg_match('|<div class="contact">\s*<p>[^<]*</p>\s*<ul>\s*<li>\s*<a[^>]*>([^<]*)</a>|isU', $shopHtml2, $m)){
+            $this->shop[SHOP_EMAIL] = $m[1];
         }
 
-        //$this->shop['SHOP_EMAIL'] = '';
-        if (preg_match('|<img [^>]*src="(/images/shoplogos/[^"]*)"|isU', $shopHtml, $m)){
-            $this->shop[SHOP_LOGO] = 'https://www.ekomi.es/' . $m[1];
+        if (preg_match('|<img class="shoplogo" [^>]*src="([^"]+)"|isU', $shopHtml, $m)){
+            $this->shop[SHOP_LOGO] = $m[1];
         }
+
+        $shopID = preg_replace('|.*info_|i', '', $url_competitor);
+        $shopID = str_replace('.html', '', $shopID);
+
+        $mainUrl = 'https://www.trustedshops.es/bewertung/new_profile_template/comments_page.php?method=all&page=1&externalShopId=' . $shopID;
+        $mainHtml = $downloader->get_html($mainUrl);
 
         //CRAWL
         $currentPage = 1;
-        $numberPages = 1;
-        if (preg_match_all('|<a [^>]*class=.pagelink[^>]*s=(\d+)\D|is', $mainHtml, $m)){
-            $numberPages = max($m[1]);
-        }
-        while ($currentPage <= $numberPages){
+        $adsFound = true;
+        while ($adsFound){
             echo "Pagina $currentPage: ".count($this->reviews)." en total\n";
-            if (preg_match_all('|<li class="commentItemWrapper">(.*)</li>|isU', $mainHtml, $mReviews)){
+            if (preg_match_all('|<div class="comment">(.*)</div>|isU', $mainHtml, $mReviews)){
                 for ($i = 0; $i < count($mReviews[1]); $i++){
                     $reviewHtml = $mReviews[1][$i];
                     $this->mapReview($reviewHtml);
                 }
+                $adsFound = true;
             }
+            else $adsFound = false;
 
             //PAGINATION
             $currentPage++;
-            $nextUrl = $url_competitor . '?s=' . $currentPage;
+            $nextUrl = str_replace('page=1', 'page=' . $currentPage, $mainUrl);
             $mainHtml = $downloader->get_html($nextUrl);
         }
 
@@ -82,23 +92,23 @@ class eKomiImporter
         );
 
         $resultsJson = json_encode($results);
-        $this->insertShopAndOpinions($resultsJson, $url_competitor);
+        $this->insertShopAndOpinions($resultsJson);
+
     }
 
     function mapReview($reviewHtml) {
-        $reviewHtml = $this->encodeHTML($reviewHtml);
         $review = array();
-        if (preg_match('|<div [^>]*class="whiteBoxT2InnerRight"[^>]*>(.*)</div>|isU', $reviewHtml, $m)){
+        if (preg_match('|<p class="the_statement">(.*)</p>|isU', $reviewHtml, $m)){
             $review[DESCRIPTION] = $this->myStripTags($m[1]);
         }
-        if (preg_match('|<div [^>]*class="smallGradeWrapper right"[^>]*>\s*<span>([^<]*)</span>|isU', $reviewHtml, $m)){
+        if (preg_match('|mini_(\d+)\.gif"|isU', $reviewHtml, $m)){
             $rating = $this->convertRating($m[1]);
             if ($rating){
                 $review[RATING] = $rating;
             }
         }
-        if (preg_match('|<div [^>]*class="commentDetails"[^>]*>[^<]*\D(\d+\.\d+\.\d{4})[^<]*\D(\d{1,2}:\d{2})[^<]*</div>|is', $reviewHtml, $m)){
-            $date = $m[1] . ' ' . $m[2];
+        if (preg_match('|<p class="meta">[^<]*\D(\d+\.\d+\.\d{4})|is', $reviewHtml, $m)){
+            $date = $m[1];
             $review[DATE] = date('Y-m-d H:i', strtotime($date));
         }
         if ($this->languange) $review[LANGUAGE] = $this->languange;
@@ -131,12 +141,10 @@ class eKomiImporter
         return iconv($this->encoding, "UTF-8", $html);
     }
 
-    function insertShopAndOpinions($json, $url_competitor){
+    function insertShopAndOpinions($json){
         $downloader = new htmlDownloader();
         $url = 'http://api.eshopinion.cat/POST/Crawler';
-        $sortida = $downloader->get_html($url, $json); //PETICION API 1ç
-        $url = 'http://api.eshopinion.cat/POST/Crawler'; //SETEAR TIENDA COMO PARSEADA
-        $sortida = $downloader->get_html($url, $url_competitor); //PETICION API 2
+        $sortida = $downloader->get_html($url, $json);
         print_r($sortida);
     }
 }
