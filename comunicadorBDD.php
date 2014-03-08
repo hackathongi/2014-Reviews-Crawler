3d40b5fbc2103d423c822c1883dc7beeedabaaec
Alex Toro <alex@volcanicinternet.com>
2014-03-08 19:13:24 +0100

    Add .gitignore

--- comunicadorBDD.php
+++ comunicadorBDD.php
@@ -9,25 +9,83 @@ include_once 'htmlDownloader.php';
 
 class comunicadorBDD
 {
+
+    /*
+     * JSON EXAMPLE
+     *
+     * ATENCION: En el elemento shop puede aparecer "shop_id", esto hace referencia a una tienda que ya existe en nuestra BDD
+     *
+    {"shop": {
+        "shop_name": "123regenwasser.de",
+        "shop_url": "http://www.123regenwasser.de",
+        "shop_url_competitor": "https://www.trustedshops.es/evaluacion/info_XE6858272B93E59E044B95103208AC479.html",
+        "shop_email": "info@dieregensammler.de",
+        "shop_phone": "06184 / 929473",
+        "shop_logo": "https://www.trustedshops.es/shoplogo/123regenwasser-de_14150.gif"
+    },
+    "reviews": [
+        {
+            "description": "Fachkundige und freundliche Beratung und prompte Zustellung der Lieferung. Kann ich nur weiter empfehlen. Auch Material und Aussehen der Regentonne entspricht voll dem Bild und somit auch den Erwartungen.",
+            "rating": "5",
+            "date": "2013-11-04 00:00",
+            "language": "de"
+        },
+        {
+            "description": "Lieferung kam wie angegeben nach 10 Tagen per Spedition. Alles in Ordnung, sehr zu empfehlen",
+            "rating": "5",
+            "date": "2013-05-13 00:00",
+            "language": "de"
+        }
+    ]
+}
+     */
+
     function insertShopAndOpinions($json, $url_competitor){
         print_r($json);
         $downloader = new htmlDownloader();
         $url = 'http://api.eshopinion.cat/POST/Crawler';
-        $sortida = $downloader->get_html($url, $json); //PETICION API 1
+        $sortida = $downloader->get_html($url, $json);
         $this->setAsParsered($url_competitor);
     }
 
-    function setAsParsered($url_competitor){
+    //Esta funcion debe insertar una nueva url de competitor en BDD... esta url tendra el booleano de is_parsered a false en la BDD
+    /*
+     * {
+    "urls": [
+        "https://www.trustedshops.es/evaluacion/info_XABB0FD76AF092B84E9C134FB0B941686.html",
+        "https://www.trustedshops.es/evaluacion/info_XCAFC31FCB4521B2D75EBD8DBD6DBE36F.html",
+        "https://www.trustedshops.es/evaluacion/info_X5D4D9AE75D508EF1527CEC45E8422981.html",
+        "https://www.trustedshops.es/evaluacion/info_X64E4DFE174E2650C57A1F4CF3DD29618.html",
+        "https://www.trustedshops.es/evaluacion/info_X070CD9A2293767D7381136BB336B419F.html",
+        "https://www.trustedshops.es/evaluacion/info_X4F549D6CD439588534A9152C6D1436EE.html",
+        "https://www.trustedshops.es/evaluacion/info_XC42E0681E9404EF6C009D80E50342CCD.html",
+        "https://www.trustedshops.es/evaluacion/info_XB23DCE0BEC7230018D8298D6F16F4BD5.html",
+        "https://www.trustedshops.es/evaluacion/info_XA7775533611E130D4E6C65E5A10993C3.html"
+    ]
+}
+     *
+     */
+    function insertNewCompetitors($json){
         $downloader = new htmlDownloader();
-        $url = 'http://api.eshopinion.cat/POST/Crawler/IsParsered'; //SETEAR TIENDA COMO PARSEADA
-        $sortida = $downloader->get_html($url, $url_competitor); //PETICION API 2
+        $url = 'http://api.eshopinion.cat/POST/NewCompetitor';
+        $sortida = $downloader->get_html($url, $json);
     }
 
-    function insertNewCompetitorUrl($url_new_competitor){
+    //Esta funcion setea como parseada una url competitor de una tienda (is_parsered a true)
+
+    function setAsParsered($url_competitor){
         $downloader = new htmlDownloader();
-        $url = 'http://api.eshopinion.cat/POST/Crawler/NewCompetitor'; //SETEAR TIENDA COMO PARSEADA
-        $sortida = $downloader->get_html($url, $url_new_competitor); //PETICION API 2
+        $url = 'http://api.eshopinion.cat/POST/IsParsered';
+        $sortida = $downloader->get_html($url, $url_competitor);
     }
+
+    //Esta funcion coje el siguiente url competitor que no esta ya parseado
+    function getNextShopCompetitor(){
+        $downloader = new htmlDownloader();
+        $url = 'http://api.eshopinion.cat/POST/Crawler/GetNextShopCompetitor';
+        return $url_competitor = $downloader->get_html($url);
+    }
+
 }
 
 ?>
--- trustedShopsURL.php
+++ trustedShopsURL.php
@@ -3,9 +3,13 @@
 //includes
 include_once 'htmlDownloader.php';
 include_once 'constants.php';
+include_once 'comunicadorBDD.php';
 
 //RUN
-parseShop('http://www.trustedshops.es/b2c_int/getdata.php?module=search_shop2&format=json&q=.');
+$bot = new TrustedShopsURL();
+$bot->parseShop('http://www.trustedshops.es/b2c_int/getdata.php?module=search_shop2&format=json&q=.');
+
+//CLASS
 
 class TrustedShopsURL {
   function parseShop($url){
@@ -13,31 +17,36 @@ class TrustedShopsURL {
     $html = $downloader->get_html($url);
      
     if (preg_match('|\[(.*)$|is', $html, $adInfo)){
-      if(preg_match_all('|"(\d+)"|is'$adInfo[1], $id) {
-        $shops = array();
-        for ($i = 0; $i < count($id); $i++) {
-          $link = 'http://www.trustedshops.es/b2c_int/getdata.php?module=shopData&format=json';
-          for ($j = 0; $j < 85; $j++) {
-            $link += '&shops%5B%5D='.$id[$i];
+      if(preg_match_all('|"(\d+)"|is', $adInfo[1], $id)) {
+          $index = 0;
+          $shops = array();
+          while ($index < count($id[1])){
+              $link = 'http://www.trustedshops.es/b2c_int/getdata.php?module=shopData&format=json';
+              $end = $index + 85;
+              for ($i = $index; $i < $end; $i++) {
+                  $index++;
+                  if (isset ($id[1][$index])) $link .= '&shops[]='.$id[1][$index];
+              }
+              $this->parseShopURL($link, $shops);
+              $json = json_encode(array("urls" => $shops));
+              $com = new comunicadorBDD();
+              $com->insertNewCompetitors($json);
+
           }
-          $shops = parseShopURL($link,$shops);
-        }
-        print_r(json_encode($shops));
       }
     }
   }
 
-  function parseShopURL($url, $shops) {
+  function parseShopURL($url, &$shops) {
     $downloader = new htmlDownloader();
+
     $html = $downloader->get_html($url);
       
-    if (preg_match_all('|"url":"([^"]+)"|is', $html, $adInfo)){
-      for ($i = 0; $i < count($adInfo); $i++) {
-        $shopCode = substr($adInfo[$i],strrpos($adInfo[$i], '_'));
-        $shop = 'https://www.trustedshops.es/evaluacion/info'.$adInfo[$i]; 
-        array_push($shops,$shop);
+    if (preg_match_all('|"tsId":"([^"]+)"|is', $html, $adInfo)){
+      for ($i = 0; $i < count($adInfo[1]); $i++) {
+        $shopUrl = 'https://www.trustedshops.es/evaluacion/info_'.$adInfo[1][$i].'.html';
+        $shops[] = $shopUrl;
       }
-      return $shops;
     }    
   }
 }
