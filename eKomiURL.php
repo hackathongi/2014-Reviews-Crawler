283fee632a0a4ba68abafcb39f021a33e76ad03f
xavi15t <xavi15t@gmail.com>
2014-03-08 21:21:24 +0100

    ekomiComBDD

--- eKomiURL.php
+++ eKomiURL.php
@@ -3,10 +3,13 @@
 //includes
 include_once 'htmlDownloader.php';
 include_once 'constants.php';
-//RUN
-class eKomiURL {
+include_once 'comunicadorBDD.php';
 
-  function parseReviews('https://www.google.es/search?q=site%3Ahttps%3A%2F%2Fwww.ekomi.es%2Ftestimonios&oq=site%3Ahttps%3A%2F%2Fwww.ekomi.es%2Ftestimonios&aqs=chrome.0.69i59j69i58.9087j0j4&sourceid=chrome&espv=210&es_sm=122&ie=UTF-8#q=site:www.ekomi.es/testimonios&start=');
+//RUN
+$bot = new eKomiURL();
+$bot -> nextGoogle('https://www.google.es/search?q=site%3Ahttps%3A%2F%2Fwww.ekomi.es%2Ftestimonios&oq=site%3Ahttps%3A%2F%2Fwww.ekomi.es%2Ftestimonios&aqs=chrome.0.69i59j69i58.9087j0j4&sourceid=chrome&espv=210&es_sm=122&ie=UTF-8#q=site:www.ekomi.es/testimonios&start=');
+
+class eKomiURL {
 
   function nextGoogle($url){
     $finished = 0;
@@ -18,14 +21,15 @@ class eKomiURL {
 
       if (preg_match_all('|<cite class="_jd">(https://www\.ekomi\.es/testimonios-[^<]+)</cite>|is', $html, $adInfo)){
         for ($i = 0; $i < count($adInfo); $i++) {
-          array_push($review,$adInfo[$i]);
+          $review[] = $adInfo[1][$i];
         }
       } else {
         $finished = 1;
       }
     }
-    $json = json_encode($review);
-    print_r($json);
+    $json = json_encode(array("urls" => $review));
+    $com = new comunicadorBDD();
+    $com->insertNewCompetitors($json);
   }
 }
 
